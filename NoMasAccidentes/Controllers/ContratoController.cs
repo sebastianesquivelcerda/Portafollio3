360f7809a3cd2c19ff655dccb5bd9adffce2c607
Sebastian Esquivel <seba.esquivel@duocuc.cl>
2021-11-28 21:44:10 -0300

    creacion

---
+++ NoMasAccidentes/Controllers/ContratoController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NoMasAccidentesNegocio;
+
+
+namespace NoMasAccidentes.Controllers
+{
+    public class ContratoController : Controller
+    {
+        // GET: Contrato
+        public ActionResult Index()
+        {
+            ViewBag.Contratos = new Contrato().ReadAll();
+            return View();
+        }
+
+        // GET: Contrato/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Contrato/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contrato/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "fecha_Creacion,rut,fecha_Termino,,texto_contrato")]Contrato contrato)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                contrato.Save();
+                TempData["mensaje"] = "Guardado Correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(contrato);
+            }
+        }
+
+        // GET: Contrato/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+
+            Contrato p = new Contrato().find(id);
+
+            if (p == null)
+            {
+                TempData["mensaje"] = "El Contrato no existe";
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        // POST: Contrato/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "ID_Contrato,fecha_Creacion,rut,fecha_Termino,,texto_contrato ")]Contrato contrato)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                contrato.Update();
+                TempData["mensaje"] = "modificado correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contrato/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            if (new Contrato().find(id) == null)
+            {
+                TempData["mensaje"] = "no existe el producto";
+                return RedirectToAction("Index");
+            }
+
+            if (new Contrato().Delete(id))
+            {
+                TempData["mensaje"] = "Eliminado Correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // POST: Contrato/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NoMasAccidentesNegocio/Contrato.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoMasAccidentesDALC;
+using NoMasAccidentesNegocio;
+namespace NoMasAccidentesNegocio
+{
+   public class Contrato
+    {
+        public int id_Contrato { get; set; }
+        public DateTime fecha_Creacion_Contrato { get; set; }
+        public string empresa_Rut { get; set; }
+        public DateTime fecha_Termino_Contrato { get; set; }
+        public string texto_Contrato { get; set; }
+
+        NoMasAccidentesEntitites db = new NoMasAccidentesEntitites();
+
+        public List<Contrato> ReadAll()
+        {
+            return this.db.CONTRATO.Select(p => new Contrato()
+            {
+                id_Contrato = (int)p.ID_CONTRATO,
+                fecha_Creacion_Contrato = (DateTime)p.FECHA_CREACION_CONTRATO,
+                empresa_Rut = p.EMPRESA_RUT,
+                fecha_Termino_Contrato = (DateTime)p.FECHA_TERMINO_CONTRATO,
+                texto_Contrato = p.TEXTO_CONTRATO
+
+
+            }).ToList();
+
+        }
+
+        public bool Save()
+       {
+           try
+           {
+
+
+
+                db.SP_CREATE_CONTRATO(this.fecha_Creacion_Contrato, this.empresa_Rut, this.fecha_Termino_Contrato, this.texto_Contrato);
+                return true;
+
+
+           }
+           catch (Exception)
+           {
+
+               return false;
+           }
+       }
+
+    public Contrato find(int id)
+        {
+            return this.db.CONTRATO.Select(p => new Contrato()
+            {
+                id_Contrato = (int)p.ID_CONTRATO,
+                fecha_Creacion_Contrato = (DateTime)p.FECHA_CREACION_CONTRATO,
+                empresa_Rut = p.EMPRESA_RUT,
+                fecha_Termino_Contrato = (DateTime)p.FECHA_TERMINO_CONTRATO,
+                texto_Contrato = p.TEXTO_CONTRATO
+
+
+            }).Where(p => p.id_Contrato == id).FirstOrDefault();
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                db.SP_UPDATE_PRODUCTO(this.id_Contrato, this.fecha_Creacion_Contrato,
+                                      this.empresa_Rut, this.fecha_Termino_Contrato,
+                                      this.texto_Contrato);
+
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                db.SP_DELETE_CONTRATO(id);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        
+    }
+
+    
+}
